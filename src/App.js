d31a26d80815e72d3ad001061a59ad9f1e7e66f8
tonezone108 <tonezone108@gmail.com>
2020-01-21 20:29:37 -0600

    removed dead code

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import Todo from "./components/Todo";
 
-// const vals = ["name1", "name2", "name3"];
-
 function App() {
   return (
     <div className="App">
-      {/* {vals.map(value => {
-        return <Todo name={value} />;
-      })} */}
       <Todo />
     </div>
   );
